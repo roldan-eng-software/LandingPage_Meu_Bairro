abd8733f283aa1d9038f8235cd2c11f0d50a8cf4
roldan-eng-software <roldan.eng.software@gmail.com>
2026-01-06 21:08:39 -0300

    Fix React Router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const App = () => {
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/LandingPage_Meu_Bairro">
           <Routes>
             <Route path="/" element={<Index />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
